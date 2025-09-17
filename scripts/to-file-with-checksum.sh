e61aef5de6bffef656d2fea756c862818df0d97b
madebytimo <t.schnaible@madebytimo.de>
2025-09-18 00:20:14 +0200

    use mbuffer instead of tee, add option to use file instead stdin

--- scripts/to-file-with-checksum.sh
+++ scripts/to-file-with-checksum.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-DEPENDENCIES=(sha256sum tee)
+DEPENDENCIES=(sha256sum mbuffer)
+INPUT="-"
 OUTPUT=""
+SILENT=false
 
 # Help message
 for ARGUMENT in "$@"; do
@@ -11,7 +13,9 @@ for ARGUMENT in "$@"; do
         echo "Saves piped data into a file and calculate the sha256sum for it."
         echo "The checksum is saved next to the file with \".sha256sum\" as suffix."
         echo "ARGUMENT can be"
+        echo "    --input FILE The input file path. If not set, data is read from stdin."
         echo "    --output FILE The output file path."
+        echo "    --silent Suppress all messages except errors."
         echo "Exit codes:"
         echo "     0: Everything fine."
         echo "     1: General error."
@@ -34,9 +38,14 @@ done
 
 # Check arguments
 while [[ -n "$1" ]]; do
-    if [[ "$1" == "--output" ]]; then
+    if [[ "$1" == "--input" ]]; then
+        shift
+        INPUT="$1"
+    elif [[ "$1" == "--output" ]]; then
         shift
         OUTPUT="$1"
+    elif [[ "$1" == "--silent" ]]; then
+        SILENT=true
     else
         echo "Error: Unknown argument: \"$1\""
         exit 13
@@ -53,12 +62,22 @@ if [[ ! -d "$(dirname "${OUTPUT}")" ]]; then
     exit 14
 fi
 
-if [[ -t 0 ]]; then
+if [[ "$INPUT" != "-" ]]; then
+        if [[ ! -f "$INPUT" ]]; then
+        echo "Error: Input file does not exist: \"$INPUT\"" >&2
+        exit 14
+    fi
+elif [[ -t 0 ]]; then
     echo "Error: No data piped to the script." >&2
     exit 1
 fi
 
-tee "${OUTPUT}" \
+ADDIONAL_ARGUMENTS=()
+if [[ "$SILENT" == true ]]; then
+    ADDIONAL_ARGUMENTS+=(-q)
+fi
+
+mbuffer -e -f -i "$INPUT" -m 32M -o "${OUTPUT}" -o - "${ADDIONAL_ARGUMENTS[@]}" \
     | sha256sum \
     | sed "s|-$|$(basename "${OUTPUT}")|" \
     > "${OUTPUT}.sha256sum"
